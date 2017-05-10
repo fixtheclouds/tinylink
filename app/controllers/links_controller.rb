977f154dd8467d36cd9155473cc5071f0812f76d
Egor Romanov <unsaidxpl@gmail.com>
2017-05-10 09:15:15 +0300

    Minor refactoring

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -7,6 +7,8 @@ class LinksController < ApplicationController
   def create
     link = Link.new(link_params)
     link.short_url = generate_short_url unless params[:short_url].present?
+    link.url = sanitize_url(params[:url])
+    link.save_http_status
     if link.save
       render json: { url: "#{request.protocol + request.host_with_port}/#{link.short_url}" }
     else
@@ -28,6 +30,10 @@ class LinksController < ApplicationController
   # Produces random Base36 8-character sequence
   def generate_short_url
     ("%d%d" % [rand(100), Time.now.to_i]).to_i.to_s(36)
+  end
+
+  def sanitize_url(url)
+    url=~/^https?:\/\// ? url : "http://#{url}"
   end
 
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -3,29 +3,19 @@ class Link < ApplicationRecord
   validates :url, :short_url, presence: true
   validates_length_of :short_url, maximum: 8
   validates_uniqueness_of :short_url
-  validate :url_exists
+  validates_exclusion_of :http_status, in: [404], message: ': URL does not exist'
 
-  before_save do |link|
-    link.url = "http://#{url}" unless link.url=~/^https?:\/\//
-  end
-
-  private
-
-  def url_exists
+  def save_http_status
     begin
-      url_object = URI.parse(sanitize_url(url))
+      url_object = URI.parse(self.url)
       request = Net::HTTP.new(url_object.host, url_object.port)
       request.use_ssl = (url_object.scheme == 'https')
       path = url_object.path if url_object.path.present?
       response = request.request_head(path || '/')
-      errors.add(:url, 'URL does not exist') if response.code == 404
-    rescue SocketError => e
-      errors.add(:url, "Error while fetching URL: #{e.to_s}")
+      self.http_status = response.code
+    rescue SocketError
+      self.http_status = 404
     end
-  end
-
-  def sanitize_url(url)
-    url=~/^https?:\/\// ? url : "http://#{url}"
   end
 
 end
