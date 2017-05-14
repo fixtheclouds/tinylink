da6b5b4390744a315c41c932251519a1feca0772
Egor Romanov <unsaidxpl@gmail.com>
2017-05-14 10:28:40 +0300

    Set 301 for redirects

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -18,7 +18,7 @@ class LinksController < ApplicationController
 
   def redirect
     link = Link.find_by_short_url(params[:short_url])
-    redirect_to link.url
+    redirect_to link.url, status: 301
   end
 
   private
